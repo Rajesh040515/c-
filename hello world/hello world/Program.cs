12f610762d2364876cf63dd3a56385dba518a882
Rajesh <rajesh.facile@outlook.com>
2021-03-12 04:39:38 -0500

    class demo

--- hello world/hello world/Program.cs
+++ hello world/hello world/Program.cs
@@ -1,21 +1,19 @@
 ﻿using System;
-
+using hello_world.Math;
 namespace hello_world
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-//postfix 
-          int a;
-            a = 50;
-            Console.WriteLine(a++);//output 50
-//prefix
-            int a;
-            a = 50;
-            Console.WriteLine(++a);//output 51
-
-
+            var r = new person();
+            r.firstname = "Rajesh";
+            r.lastname = "patel";
+            r.introduce();
+            calculator calculator = new calculator();
+            var result = calculator.add(1, 2);
+            System.Console.WriteLine(result);
         }
     }
 }
