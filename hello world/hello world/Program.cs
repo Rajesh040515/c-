a92b1b230d2d60818c37fccd587e4066e815951c
Rajesh <rajesh.facile@outlook.com>
2021-03-12 06:19:28 -0500

    string demo

--- hello world/hello world/Program.cs
+++ hello world/hello world/Program.cs
@@ -7,23 +7,15 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            var number = new int[3];
-            number[0] = 1;
+           var firstname = "rajesh";
+            var lastname = "patel";
+            var fullname = firstname + " " + lastname;
+            Console.WriteLine(fullname);
+            var myfullname = string.Format("my name is {0} {1}", firstname, lastname);
 
-            Console.WriteLine(number[0]);
-            Console.WriteLine(number[1]);
-            Console.WriteLine(number[2]);
-
-            var flags = new bool[3];
-            flags[0] = true;
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
-        
-            
-            var str = new string[2] { "rajesh", "patel" };
-            Console.WriteLine(str[0]);
-        
+            var names = new string[2] { "a", "b" };
+            var formatnames = string.Join(",", names);
+            Console.WriteLine(formatnames);
         
         }
     }
