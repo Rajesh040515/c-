48f2367094ddaedd914655727b8e717f60f3bd84
Rajesh <rajesh.facile@outlook.com>
2021-03-12 02:14:33 -0500

    demo variable declaration

---
+++ hello world/hello world/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                byte number = 25;
+            var count = 10;
+            float totalprice = 20.10f;
+            char character = 'a';
+            string fname = "Rajesh";
+            bool isworking = true;
+            
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalprice);
+            Console.WriteLine(character);
+            Console.WriteLine(fname);
+            Console.WriteLine(isworking);
+
+               
+                Console.WriteLine(number);
+          
+        }
+    }
+}
